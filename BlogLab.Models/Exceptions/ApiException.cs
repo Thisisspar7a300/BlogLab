125b4562fbada6e4d3330a60b4270167afe84018
Thisisapra7a300 <mikezinke94@gmail.com>
2024-12-10 13:31:56 -0500

    Models project compete Repository project created with account repisitory

---
+++ BlogLab.Models/Exceptions/ApiException.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Exceptions
+{
+    internal class ApiException
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public override string ToString() => JsonConvert.SerializeObject(this);
+    }
+}
