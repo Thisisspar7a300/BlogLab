88f08bb2b26b58a45493c07f15d28c3755413cc4
Thisisapra7a300 <mikezinke94@gmail.com>
2024-12-13 18:35:17 -0500

    Service project completed.

--- BlogLab.web/Startup.cs
+++ BlogLab.web/Startup.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using BlogLab.Models.Settings;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 public class Startup
 {
+    public IConfiguration Configuration { get; }
+    public Startup(IConfiguration config)
+    {
+        Configuration = config;
+    }
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddControllers(); // Add services for controllers
+        services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
